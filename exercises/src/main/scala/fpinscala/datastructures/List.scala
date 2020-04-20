1e27f35e008c4ebc0218246f2838b4d34d37a32b
Edward Huang <edwardgunawan880@yahoo.com>
2020-04-19 23:28:42 -0400

    data structure foldRight

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -36,11 +38,11 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Nil => a2
       case Cons(h,t) => Cons(h, append(t, a2))
     }
-
+  // it is not tail recursive
   def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
       case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f)) // because you need to TRAVERSE until the end to know to call the function from the end value
     }
 
   def sum2(ns: List[Int]) =
@@ -50,19 +52,78 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("Tail is empty")
+    case Cons(head, tail) => tail
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Cons(_, tail) => Cons(h, tail)
+    case Nil => sys.error("set Head is empty")
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = (n, l) match {
+    case (0, lst) => lst
+    case (n, Cons(_, tail)) => drop(tail, n-1)
+    case _ => Nil
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(head, tail) if f(head) => dropWhile(tail,f)
+    case _ => l
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("init of empty list")
+    case Cons(h,tail) => Cons(h,init(tail))
+    case Cons(_, Nil) => Nil
+  }
+  /*
+    Compute the length of the list using foldRight
+   */
+  def length[A](l: List[A]): Int = foldRight(l,0)((_, acc) => acc + 1)
 
-  def length[A](l: List[A]): Int = ???
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Cons(h, tail) => foldLeft(tail, f(z, h))(f)
+    case Nil => z
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  /*
+    Write sum,product and a function to compute the length of a list using foldLeft
+   */
+  def sumWithFoldLeft(ls:List[Int]):Int = foldLeft(ls, 0)(_ + _)
+  def productWithFoldLeft(ls:List[Int]):Int = foldLeft(ls, 1)(_ * _)
+  def lengthWithFoldLeft[A](ls:List[A]):Int = foldLeft(ls,0)((acc, _) => acc +1)
+
+  /*
+    Write a function taht returns the reverse of a list (given List(1,2,3) it returns List(3,2,1)). See if
+    you can write it using fold
+   */
+  def reverse[A](ls:List[A]): List[A] = foldLeft(ls,List[A]())((acc,head) => Cons(head,acc))
+
+  /*
+    Implement foldLeft in terms of foldRight? How about the other way around? Implementing foldRight
+    via foldLeft is useful beacuse it lets use implement foldRight
+    Great Explanation: https://stackoverflow.com/questions/17136794/foldleft-using-foldright-in-scala
+    What happened is that it does function composition from right associativity to left
+    Compose is the return on the right side will be the input on the left side
+    When you are doing foldLeft, it is the same as doing it foldRight beause it will do right associative.
+    so from f(1,(2,f(z, 3))) to f(f(f(z,1),2),3)
+
+    For each element a of l we take the partial application b => f(b,a), which is a function B => B. Then, we compose
+    all these functions in such a way that the function corresponding to the rightmost element (with which we start the
+    traversal) is at far left in the composition chain. Finally, we apply the big composed function on z.
+   */
+  def foldLeftFromFoldRight[A,B](as:List[A], s:B)(f:(A,B) => B):B = {
+    def h(a: A, g:B => B): (B => B) = g compose ((x:B) => f(a,x)) // equal to (x:B) => g(f(a, x))
+    foldRight(as, (b:B) => b)(h)(s)
+  }
+
+  def foldRightFromFoldLeft[A,B](as:List[A], s:B)(f:(B, A) => B): B = {
+    def h(g: B => B, a:A):(B => B) = (b:B) => g(f(b,a))
+    foldLeft(as,(b:B) => b)(h)(s)
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
